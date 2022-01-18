364c0db362b986bd59f880550c80f2ff2d108e48
R.J.PRAVEEN <97513071+Praveenrj17@users.noreply.github.com>
2022-01-18 21:09:52 +0530

    Add files via upload

---
+++ Assignment 4(17-01-2022)/Comparison.java
@@ -0,0 +1,14 @@
+class Comparison
+{
+public static void main(String args[])
+{
+int a=10,b=20;
+System.out.println("The Comparison between two numbers is");
+System.out.println(a == b);
+System.out.println(a != b);
+System.out.println(a > b);
+System.out.println(a < b);
+System.out.println(a <= b);
+System.out.println(a >= b);
+}
+}
---
+++ Assignment 4(17-01-2022)/Signed.java
@@ -0,0 +1,14 @@
+class Signed
+{
+public static void main(String args[])
+{
+int a=10,b=-100;
+System.out.println("The Comparison of Signed and Unsigned number is");
+System.out.println(a == b);
+System.out.println(a != b);
+System.out.println(a > b);
+System.out.println(a < b);
+System.out.println(a <= b);
+System.out.println(a >= b);
+}
+}
