56b5dbeadc4aac44c8e980489244014431889c79
R.J.PRAVEEN <97513071+Praveenrj17@users.noreply.github.com>
2022-01-18 21:02:53 +0530

    Add files via upload

---
+++ Assignment 1(07-01-2022)/Division.java
@@ -0,0 +1,9 @@
+class Division
+{
+public static void main(String args[])
+{
+int a=12,b=6,sum;
+sum=a/b;
+System.out.println(sum);
+}
+}
---
+++ Assignment 1(07-01-2022)/Myhello.java
@@ -0,0 +1,7 @@
+class Myhello
+{
+public static void main(String args[])
+{
+System.out.println("hello world");
+}
+}
