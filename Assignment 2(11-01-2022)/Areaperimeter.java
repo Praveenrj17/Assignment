838d428626b5df8673da93aadb5299a12e07deb9
R.J.PRAVEEN <97513071+Praveenrj17@users.noreply.github.com>
2022-01-18 21:09:02 +0530

    Add files via upload

---
+++ Assignment 2(11-01-2022)/Areaperimeter.java
@@ -0,0 +1,12 @@
+class Areaperimeter
+{
+public static void main(String args[])
+{
+int height=7;
+int width=6;
+int perimeter= 2*(height+width);
+int area= width*height;
+System.out.println(perimeter);
+System.out.println(area);
+}
+}
