86edf7c00fdd84805347ba228a05d9f2942ec533
R.J.PRAVEEN <97513071+Praveenrj17@users.noreply.github.com>
2022-01-19 20:07:07 +0530

    Add files via upload

---
+++ Assignment 5(18-01-2022)/Greatestnumber.java
@@ -0,0 +1,14 @@
+class Greatestnumber 
+{  
+public static void main(String[] args)   
+{    
+int a=48, b=78, c=91;   
+if(a>=b && a>c)  
+System.out.println(a+" is the largest Number");    
+else if (b>a && b>c)  
+System.out.println(b+" is the largest Number");  
+else    
+System.out.println(c+" is the largest number");  
+}  
+}  
+	
---
+++ Assignment 5(18-01-2022)/Naturalnumber.java
@@ -0,0 +1,8 @@
+class Naturalnumber
+{
+public static void main(String args[])
+{	
+for(int i=1;i<=10;i++)
+System.out.println("The natural numbers are:" + i);
+}
+}
